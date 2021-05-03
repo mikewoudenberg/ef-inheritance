6d6a3ff229825eae208541428b9cdc7c919b988f
Manuel Riezebosch <mriezebosch@gmail.com>
2021-05-03 22:01:23 +0200

    feat(test): simple repro without service

--- backend-tests/OrderApiTest.cs
+++ backend-tests/OrderApiTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Mime;
@@ -53,7 +54,8 @@ namespace backend_tests
         {
             //Arrange
             var (client, dbContext) = IntegrationTestBootstrapper.Build();
-            var order = new BreadOrder(Guid.NewGuid(), "someone", "here", new() { new BreadOrderLine(Guid.NewGuid(), 12345, "Must be good", new BreadPreferences(true, false)) });
+            var order = new BreadOrder(Guid.NewGuid(), "someone", "here",
+                new() {new BreadOrderLine(Guid.NewGuid(), 12345, "Must be good", new BreadPreferences(true, false))});
             dbContext.Orders.Add(order);
             dbContext.SaveChanges();
 
@@ -64,6 +66,65 @@ namespace backend_tests
             result.EnsureSuccessStatusCode();
             var returnedOrder = JObject.Parse(await result.Content.ReadAsStringAsync());
             returnedOrder["orderLines"].Last()["input"].Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Insert()
+        {
+            var databaseName = Guid.NewGuid().ToString();
+            using (var context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName).Options))
+            {
+                await context.Orders.AddAsync(new BreadOrder(Guid.NewGuid(), "Mike", "here", new List<OrderLine>
+                {
+                    new BreadOrderLine(new Guid("d21b08e4-16f4-4ae9-83db-b1fc7a10c4df"), 14713499, "bring it home",
+                        new BreadPreferences(true, false))
+                }));
+
+                await context.SaveChangesAsync();
+            }
+            
+            using (var context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName).Options))
+            {
+                var order = context.Orders.Include(o => o.OrderLines).Single();
+                order.OrderLines.Should().HaveCount(1);
+                order.OrderLines.Last().As<BreadOrderLine>().Input.Should().NotBeNull();
+            }
+        }
+        
+        [Fact]
+        public async Task Update()
+        {
+            var databaseName = Guid.NewGuid().ToString();
+            var id = Guid.NewGuid();
+
+            await using (var context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName).Options))
+            {
+                await context.Orders.AddAsync(new BreadOrder(id, "Mike", "here"));
+                await context.SaveChangesAsync();
+            }
+
+            await using (var context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName).Options))
+            {
+                var order = await context.Orders.FindAsync(id);
+                var line = new BreadOrderLine(new Guid("d21b08e4-16f4-4ae9-83db-b1fc7a10c4df"), 14713499, "bring it home", new BreadPreferences(true, false));
+                order.OrderLines.Add(line);
+
+                context.Attach(line).State = EntityState.Added;
+
+                await context.SaveChangesAsync();
+            }
+
+            await using (var context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName).Options))
+            {
+                var order = context.Orders.Include(o => o.OrderLines).Single();
+                order.OrderLines.Should().HaveCount(1);
+                order.OrderLines.Last().As<BreadOrderLine>().Input.Should().NotBeNull();
+            }
         }
     }
 }
